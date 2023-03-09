135c303daa7cf7c463cb146733dc4af7668d8b99
phayeon <phayeon1222@gmail.com>
2023-03-09 16:00:51 +0900

    docker db 권한 부여

--- services/frontend/vue.config.js
+++ services/frontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    allowedHosts: ['localhost:8080'],
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000',
+        ws: true,
+        changeOrigin: true
+      }
+    },
+    hot: true
+  }
 })
