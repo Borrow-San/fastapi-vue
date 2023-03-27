cecb5c832bd3633ddc893ee17a55e3c74d481c68
seongbae103 <Seongbae103@gmail.com>
2023-03-27 09:47:53 +0900

    03-27 config

--- services/frontend/vue.config.js
+++ services/frontend/vue.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     host: '0.0.0.0',
-    allowedHosts: ["www.phayeon.site", "phayeon.site"],
+    allowedHosts: ["www.borrowsan.shop", "borrowsan.shop"],
     proxy: {
       '/api': {
-        target: 'http://phayeon.site:8000',
+        target: 'http://borrowsan.shop:8000',
         ws: true,
         changeOrigin: true
       }
